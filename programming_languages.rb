e650ef19da54597b1c3466b4fbdd47746c4da87f
Ferris Boran <ferris.boran@gmail.com>
2019-03-14 02:11:14 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |att, style|
+    style.each do |language, type|
+      if !new_hash[language]
+        new_hash[language] = {}
+      end
+      type.each do |key, value|
+        new_hash[language][key] = value
+      end
+      if !new_hash[language][:style]
+        new_hash[language][:style] = []
+      end
+      new_hash[language][:style] << att
+    end
+  end
+  new_hash
 end
